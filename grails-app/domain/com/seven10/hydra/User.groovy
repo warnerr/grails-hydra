6e7e4b8227e95dda9f5cd644bf106470f4d9d7eb
Rob Warner <rwarner@seven10storage.com>
2016-04-28 09:07:16 -0400

    Security added.

---
+++ grails-app/domain/com/seven10/hydra/User.groovy
@@ -0,0 +1,57 @@
+package com.seven10.hydra
+
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.ToString
+import org.bson.types.ObjectId
+
+@EqualsAndHashCode(includes='username')
+@ToString(includes='username', includeNames=true, includePackage=false)
+class User implements Serializable {
+
+	private static final long serialVersionUID = 1
+
+	transient springSecurityService
+
+	ObjectId id
+	String username
+	String password
+	boolean enabled = true
+	boolean accountExpired
+	boolean accountLocked
+	boolean passwordExpired
+
+	User(String username, String password) {
+		this()
+		this.username = username
+		this.password = password
+	}
+
+	Set<Role> getAuthorities() {
+		UserRole.findAllByUser(this)*.role
+	}
+
+	def beforeInsert() {
+		encodePassword()
+	}
+
+	def beforeUpdate() {
+		if (isDirty('password')) {
+			encodePassword()
+		}
+	}
+
+	protected void encodePassword() {
+		password = springSecurityService?.passwordEncoder ? springSecurityService.encodePassword(password) : password
+	}
+
+	static transients = ['springSecurityService']
+
+	static constraints = {
+		password blank: false, password: true
+		username blank: false, unique: true
+	}
+
+	static mapping = {
+		password column: '`password`'
+	}
+}
